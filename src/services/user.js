64c4efe3230860fc773d1efec947e71c01f8f78c
auza0002 <auzajuan1@gmail.com>
2024-04-09 11:51:23 -0400

    Utils commments completed - working on service - user done

--- src/services/user.js
+++ src/services/user.js
@@ -1,30 +1,36 @@
 "use strict";
-// todo fix grammar 
 
+// Importing Company model and error classes
 const Company = require("../models/company");
 const { NotFoundError, BadRequestError } = require("../utils/errors");
+
+// Importing function for Google Maps API address validation
 const { googleMapValidation } = require("../utils/googleMaps");
 
+// Function to retrieve all companies belonging to a specific owner
 const getAll = async (ownerId) => {
   const company = await Company.find({ ownerId });
   return company;
 };
 
+// Function to retrieve a single company by its ID
 const getOne = async (id) => {
   const foundCompany = await Company.findById(id);
   if (!foundCompany) throw new NotFoundError(`Company with id ${id} not found`);
   return foundCompany;
 };
 
+// Function to create a new company
 const create = async (companyData) => {
   const newCompany = new Company(companyData);
   await newCompany.save();
   return newCompany;
 };
 
+// Function to replace company data with new data
 const replace = async (id, companyData) => {
   if (!companyData.country || !companyData.address || !companyData.name) {
-    throw new BadRequestError("Name, address and country are required");
+    throw new BadRequestError("Name, address, and country are required");
   }
   const formatedUserLocationData = await locationUserFormatting(companyData);
   const replaceCompany = await Company.findByIdAndUpdate(
@@ -36,12 +42,13 @@ const replace = async (id, companyData) => {
       returnOriginal: false,
     }
   );
-   if (replaceCompany == null) {
-     throw new NotFoundError("Company with id " + id + " not found");
-   }
+  if (replaceCompany == null) {
+    throw new NotFoundError("Company with id " + id + " not found");
+  }
   return replaceCompany;
 };
 
+// Function to update company data with specified fields
 const update = async (id, updatedFields) => {
   if (!Object.keys(updatedFields).length) {
     throw new BadRequestError("Nothing to update");
@@ -64,6 +71,7 @@ const update = async (id, updatedFields) => {
   return updatedCompany;
 };
 
+// Function to delete a company by its ID
 const deleteOne = async (id) => {
   const deletedCompany = await Company.findByIdAndDelete(id);
   if (deletedCompany == null) {
@@ -72,6 +80,7 @@ const deleteOne = async (id) => {
   return deletedCompany;
 };
 
+// Exporting functions for external use
 module.exports = {
   getAll,
   getOne,
@@ -81,6 +90,7 @@ module.exports = {
   update,
 };
 
+// Function to format company location data using Google Maps API
 const locationUserFormatting = async (companyData) => {
   const formatedLocationData = companyData;
   const updatedLocation = await googleMapValidation(
