5dfad9bd9ba48ca8d53093897c604cddf52f11b4
auza0002 <auzajuan1@gmail.com>
2024-04-11 12:11:03 -0400

    Adding upload middleware

---
+++ src/middleware/upload.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+
+const storage = new GridFsStorage({
+  url: process.env.MONGO_URL,
+  file : (req, file)=>{
+    const match = ["image/png", "image/jpeg"];
+    if(match.indexOf(file.mimetype) === -1){
+        const fileName = `${Date.now()}-LeadUp-FileName-${file.originalname}`
+        return fileName
+    }
+    return {
+      bucketName: "photos",
+      filename: `${Date.now()}-LeadUp-FileName-${file.originalname}`,
+    };
+  }
+});
+
+module.exports = storage
--- src/routes/imagesRouter.js
+++ src/routes/imagesRouter.js
@@ -2,8 +2,11 @@
 require("dotenv").config();
 const { Router } = require("express");
 const isAuthenticated = require("../middleware/isAuthenticated");
+const upload = require("../middleware/upload")
+
 
 const imagesRouter = Router();
 
+imagesRouter.use(isAuthenticated);
 
 module.exports = imagesRouter;
