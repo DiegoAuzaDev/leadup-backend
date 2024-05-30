cd1046fb6cf302b7d5686a159a9de9eb4fe9f355
diegoauza <auzajuan1@gmail.com>
2024-05-30 10:52:06 -0400

    Adding delivery controller

---
+++ src/controllers/delivery.js
@@ -0,0 +1,13 @@
+const { BadRequestError, NotFoundError } = require("../utils/errors.js");
+const getAll = async (req, res, next) => {};
+
+const getOne = async (req, res, next) => {};
+
+const create = async (req, res, next) => {};
+
+
+module.exports = {
+    getAll,
+    getOne,
+    create,
+}
